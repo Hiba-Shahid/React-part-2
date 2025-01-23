7fbc1055d0628604fd39c2a96cb95351c804c974
Hlab2 <hibashahid678@gmail.com>
2025-01-23 17:16:51 +0500

    Add react-query-devtools and configure default query options in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App'
 import './index.css'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries:{
+      retry: 3,
+      cacheTime: 300_000, //5m
+      staleTime: 10 * 1000, //10s
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +25,7 @@ ReactDOM.createRoot(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools/>
     </QueryClientProvider>
   </React.StrictMode>,
 )
