13b57b868dfdf8c3c96e2512ae9300f34dc5a2a3
Hlab2 <hibashahid678@gmail.com>
2025-01-24 14:26:01 +0500

    Refactor TodoForm to reset input field on successful mutation and disable button during loading

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,61 +1,58 @@
-import { useRef } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { Todo } from './hooks/useTodos';
-import axios from 'axios';
-import { useQueryClient } from '@tanstack/react-query';
+import { useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
 
 const TodoForm = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const addTodo = useMutation<Todo, Error, Todo>({
-        mutationFn: (todo: Todo) =>
-            axios
-               .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
-               .then(res => res.data),
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
 
-               onSuccess: (savedTodo, newTodo) => {
-                queryClient.setQueryData<Todo[]>(['todos'], todos =>
-                  [savedTodo,... (todos || [])]
-                  )
-              },
-    });
-    const ref = useRef<HTMLInputElement>(null);
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+      if (ref.current) ref.current.value = "";
+    },
+  });
+  const ref = useRef<HTMLInputElement>(null);
 
-    return (
-        <>
-        {addTodo.error && (
-        <div className="alert alert-danger">
-          {addTodo.error.message}
-        </div>
+  return (
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
       )}
-         <form
+      <form
         className="row mb-3"
         onSubmit={(event) => {
-            event.preventDefault();
+          event.preventDefault();
 
-            if (ref.current && ref.current.value)
+          if (ref.current && ref.current.value)
             addTodo.mutate({
-                id: 0,
-                title: ref.current?.value,
-                completed: false,
-                userId: 1,
-              });
-        }}>
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+        }}
+      >
         <div className="col">
-        <input
-          ref={ref}
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={addTodo.isLoading} className="btn btn-primary">
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
+        </div>
+      </form>
     </>
-    )
-
-
-}
+  );
+};
 
 export default TodoForm;
