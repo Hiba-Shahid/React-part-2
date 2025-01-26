be87c0b658f524326d802584ff8babf8420277a3
Hlab2 <hibashahid678@gmail.com>
2025-01-26 17:55:38 +0500

    Add LoginStatus component with authentication state management

---
+++ src/state-management/LoginStatus.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useReducer } from "react";
+import authReducer from "./reducers/authReducer";
+
+const LoginStatus = () => {
+  const [user, dispatch] = useReducer(authReducer,'');
+
+  if (user)
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => dispatch({type: 'LOGOUT'})} href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+  return (
+    <div>
+      <a onClick={() => dispatch({type: 'LOGIN', username: 'Hiba.Shahid'})} href="#">
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
