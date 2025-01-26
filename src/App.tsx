318c3939c5af59cf15a879930b63800cf13ee434
Hlab2 <hibashahid678@gmail.com>
2025-01-26 17:43:21 +0500

    Add TaskList component with useReducer for task management

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import TodoList from './react-query/TodoList'
 import PostList from './react-query/PostList';
 import TodoForm from './react-query/TodoForm';
 import Counter from './state-management/Counter';
+import TaskList from './state-management/TaskList'
 
 function App() {
   return (
-   <Counter/>
+   <TaskList/>
 )
 }
 
