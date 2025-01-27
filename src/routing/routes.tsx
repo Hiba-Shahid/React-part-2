68cf1b00f49458a7040db383e7ac12104c8bc531
Hlab2 <hibashahid678@gmail.com>
2025-01-27 21:47:58 +0500

    Add Layout and NavBar components for improved routing structure

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,17 +3,18 @@ import HomePage from "./HomePage";
 import ContactPage from "./ContactPage";
 import UserList from "./UserList";
 import UserDetail from "./UserDetail";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
     {
-        path: '/', element: <HomePage/>
-    },
-    {
-        path: '/users', element: <UserList/>
-    },
-    {
-        path: '/users/:id', element: <UserDetail/>
-    },
+        path: '/',
+        element: <Layout/>,
+        children: [
+    { index: true , element: <HomePage/> },
+    { path: 'users', element: <UserList/> },
+    { path: 'users/:id', element: <UserDetail/> },
+]
+}
 ]);
 
 export default router;
