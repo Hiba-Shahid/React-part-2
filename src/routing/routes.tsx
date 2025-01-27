802f2238a82a68fbe944250e383784c86c6c33e6
Hlab2 <hibashahid678@gmail.com>
2025-01-28 00:52:14 +0500

    Add ErrorPage and LoginPage components; update routing and authentication logic

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -4,13 +4,17 @@ import ContactPage from "./ContactPage";
 import UsersPage from "./UsersPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPaage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>,
+        errorElement: <ErrorPage/>,
         children: [
     { index: true , element: <HomePage/> },
+    { path: '/login' , element: <LoginPage/> },
     { path: 'users',
          element: <UsersPage/>,
         children: [
