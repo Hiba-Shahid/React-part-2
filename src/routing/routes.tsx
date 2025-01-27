71462b330b47e2b996be02df95657779d1ea81ad
Hlab2 <hibashahid678@gmail.com>
2025-01-27 21:55:56 +0500

    Add UsersPage component and update routing to include nested user details

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import ContactPage from "./ContactPage";
-import UserList from "./UserList";
+import UsersPage from "./UsersPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 
@@ -11,8 +11,14 @@ const router = createBrowserRouter([
         element: <Layout/>,
         children: [
     { index: true , element: <HomePage/> },
-    { path: 'users', element: <UserList/> },
-    { path: 'users/:id', element: <UserDetail/> },
+    { path: 'users',
+         element: <UsersPage/>,
+        children: [
+
+            { path: ':id', element: <UserDetail/> },
+        ]
+ },
+    
 ]
 }
 ]);
